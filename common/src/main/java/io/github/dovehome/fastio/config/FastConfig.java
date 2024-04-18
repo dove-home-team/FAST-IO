df0d19365500542f07d6f4c7b2201c2b5e1bfd21
baka4n <qq474899581@gmail.com>
2024-04-18 17:26:27 +0800

    基础配置预埋

--- common/src/main/java/io/github/dovehome/fastio/config/FastConfig.java
+++ common/src/main/java/io/github/dovehome/fastio/config/FastConfig.java
@@ -3,6 +3,18 @@ package io.github.dovehome.fastio.config;
 import com.google.gson.annotations.SerializedName;
 
 public class FastConfig {
+
+    public static final FastConfig DEFAULT = new FastConfig();
+
+    static {
+        DEFAULT.A = 1;
+        DEFAULT.B = 3;
+        DEFAULT.C = 5;
+        DEFAULT.D = 30;
+        DEFAULT.E = 1;
+        DEFAULT.F = 30;
+    }
+
     @SerializedName("A")
     public int A;
 
